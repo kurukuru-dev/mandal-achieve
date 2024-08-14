991dad1d6330c85f996571dfca1b681a85eadd8c
kotarokomori <komopro.works@gmail.com>
2024-08-14 18:31:26 +0900

    色の設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+      colors: {
+        main: '#009999',
+        sub: '#F0F0F0',
+        accent: '#344B78',
+        caution: '#FFC107'
+      }
+    }
+  }
 };
 export default config;
