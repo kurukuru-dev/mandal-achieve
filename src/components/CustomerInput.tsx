6c3fd9a5f495f68b39fbc82bef937e499d4c1426
kotarokomori <komopro.works@gmail.com>
2024-08-15 20:01:23 +0900

    使い方アイコンを作成

--- src/components/CustomerInput.tsx
+++ src/components/CustomerInput.tsx
@@ -1,3 +1,4 @@
+import { CircleHelp } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Select,
@@ -12,23 +13,23 @@ import { Input } from '@/components/ui/input';
 // q: 顧客がモードを選択し、メインテーマを入力してスタートボタンを押す
 export default function CustomerInput() {
   return (
-    <div className="mt-6 flex flex-col justify-center items-center">
-      <div className="flex gap-x-2">
-        <Select>
-          <SelectTrigger className="w-60">
-            <SelectValue placeholder="モードを選択" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="create">アイデア創造</SelectItem>
-              <SelectItem value="achive">目標達成</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Input type="text" placeholder="メインテーマを入力" />
-        <Button>スタート</Button>
-      </div>
-      <button>使い方</button>
+    <div className="mt-6 flex items-center gap-x-2">
+      <Select>
+        <SelectTrigger className="w-60">
+          <SelectValue placeholder="モードを選択" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="create">アイデア創造</SelectItem>
+            <SelectItem value="achive">目標達成</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Input type="text" placeholder="メインテーマを入力" />
+      <Button>スタート</Button>
+      <button title="使い方" className="h-6 w-6">
+        <CircleHelp className="text-accent hover:text-accent-hover" />
+      </button>
     </div>
   );
 }
