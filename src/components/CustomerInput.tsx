f36db9c6bec36678f83318af788c83117bba285f
kotarokomori <komopro.works@gmail.com>
2024-08-15 19:18:12 +0900

    inputコンポーネント作成

--- src/components/CustomerInput.tsx
+++ src/components/CustomerInput.tsx
@@ -8,25 +8,27 @@ import {
   SelectLabel,
   SelectItem
 } from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
 
 // q: 顧客がモードを選択し、メインテーマを入力してスタートボタンを押す
 export default function CustomerInput() {
   return (
-    <div className="mt-4 flex flex-col justify-center items-center">
-      <Select>
-        <SelectTrigger className="w-60">
-          <SelectValue placeholder="使用方法を選択してください" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>使用方法</SelectLabel>
-            <SelectItem value="create">アイデア創造</SelectItem>
-            <SelectItem value="achive">目標達成</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <input type="text" placeholder="メインテーマを入力" />
-      <Button>スタート</Button>
+    <div className="mt-6 flex flex-col justify-center items-center">
+      <div className="flex gap-x-2">
+        <Select>
+          <SelectTrigger className="w-60">
+            <SelectValue placeholder="モードを選択" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="create">アイデア創造</SelectItem>
+              <SelectItem value="achive">目標達成</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Input type="text" placeholder="メインテーマを入力" />
+        <Button>スタート</Button>
+      </div>
       <button>使い方</button>
     </div>
   );
