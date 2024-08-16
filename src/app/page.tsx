88dbf3d382e34d5143fbf3c201a66b0600bdbc9b
kotarokomori <komopro.works@gmail.com>
2024-08-16 21:59:42 +0900

    Footer途中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CustomerInput from '@/components/CustomerInput';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -9,23 +10,8 @@ export default function Home() {
           マンダラートを使用して「アイデアの創造」「目標達成」をサポートするアプリ
         </h2>
         <CustomerInput />
+        <Footer />
       </main>
-      <footer className="w-full text-center mx-auto absolute left-0 bottom-0">
-        <p>© 2024 Your Name. All rights reserved.</p>
-        <nav className="flex justify-center space-x-4">
-          <a href="/privacy-policy" className="hover:underline">
-            プライバシーポリシー
-          </a>
-          <a href="/terms-of-service" className="hover:underline">
-            利用規約
-          </a>
-          <a href="/contact" className="hover:underline">
-            お問い合わせ
-          </a>
-        </nav>
-        <p>SNS</p>
-        <p className="mt-2 text-sm">Version 1.0.0</p>
-      </footer>
     </div>
   );
 }
