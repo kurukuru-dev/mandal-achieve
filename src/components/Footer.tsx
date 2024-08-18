f2f9058b2ec8f4f2ead994d8143690c7db10b1e2
kotarokomori <komopro.works@gmail.com>
2024-08-19 00:15:42 +0900

    Footer作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,56 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Footer() {
   return (
-    <footer className="w-full text-center mx-auto absolute left-0 bottom-0">
-      <p>© 2024 kurukuru-dev</p>
-      <nav className="flex justify-center space-x-4">
-        <a href="/privacy-policy" className="hover:underline">
-          プライバシーポリシー
-        </a>
-        <a href="/terms-of-service" className="hover:underline">
-          利用規約
-        </a>
-        <a href="/contact" className="hover:underline">
-          お問い合わせ
-        </a>
-      </nav>
-      <a href="#">Xのアカウント</a>
+    <footer className="absolute bottom-0 left-0 mx-auto w-full bg-primary p-2 text-sm text-white">
+      <div className="flex flex-col gap-y-1 text-center">
+        <p>© 2024 kurukuru-dev</p>
+        <div className="flex h-6 justify-center gap-x-16">
+          <nav>
+            <Link
+              href="/disclaimer"
+              className="underline-offset-2 hover:underline"
+            >
+              免責事項
+            </Link>
+            <Link
+              href="/faq"
+              className="ml-4 underline-offset-2 hover:underline"
+            >
+              FAQ
+            </Link>
+          </nav>
+          <div className="flex h-6">
+            <Link
+              href="#"
+              className="relative w-6 rounded-full border-2"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/x.svg"
+                alt="X(旧Twitter)のロゴ"
+                width={11.736}
+                height={12}
+                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+              />
+            </Link>
+            <Link
+              href="https://github.com/kurukuru-dev/mandal-achieve"
+              className="relative ml-4 w-6 rounded-full border-2"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/github.svg"
+                alt="GitHubのロゴ"
+                width={15.3}
+                height={15}
+                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
