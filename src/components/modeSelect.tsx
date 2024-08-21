40b1a520063763f87aaa98a4b65d3e9a309745b4
kotarokomori <komopro.works@gmail.com>
2024-08-22 01:28:26 +0900

    modeSelectコンポーネント作成

---
+++ src/components/modeSelect.tsx
@@ -0,0 +1,32 @@
+import MODE from '@/const/mode';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem
+} from '@/components/ui/select';
+
+import { ModeValueLiteral } from '@/types/mode';
+
+type Props = {
+  mode?: ModeValueLiteral;
+  className: string;
+};
+
+export default function ModeSelect({ mode, className }: Props) {
+  return (
+    <Select defaultValue={mode} name="mode">
+      <SelectTrigger className={className}>
+        <SelectValue placeholder="モードを選択" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value={MODE.CREATE}>アイデア発想</SelectItem>
+          <SelectItem value={MODE.ACHIVE}>目標達成</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
