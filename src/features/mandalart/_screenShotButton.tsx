f3ce5b392876a94f210ba6b1b1cc6a7219e80cab
kotarokomori <komopro.works@gmail.com>
2024-09-12 01:37:05 +0900

    dom-to-imageライブラリを使うように変更

--- src/features/mandalart/_screenShotButton.tsx
+++ src/features/mandalart/_screenShotButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Camera } from 'lucide-react';
+import domtoimage from 'dom-to-image';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/components/ui/toast';
 
@@ -10,6 +11,22 @@ export default function ScreenShotButton() {
   const handleScreenShot = () => {
     const target = document.getElementById('mandalrtWrap');
     if (!target) return null;
+
+    domtoimage
+      .toSvg(target)
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'mandalart.svg';
+        link.href = dataUrl;
+        link.click();
+      })
+      .then(() => {
+        toast({ title: 'スクリーンショットを保存しました' });
+      })
+      .catch((e) => {
+        toast({ title: 'スクリーンショットの保存に失敗しました', variant: 'failed' });
+        console.error(e);
+      });
   };
 
   return (
