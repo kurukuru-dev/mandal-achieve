d21f4f4cf565462b35afa6254405673964165c3d
kotarokomori <komopro.works@gmail.com>
2024-09-13 01:44:36 +0900

    howtobuttonを除却

--- src/components/howToButton.tsx
+++
@@ -1,9 +0,0 @@
-import { CircleHelp } from 'lucide-react';
-
-export default function HowToButton() {
-  return (
-    <button title="使い方" className="h-6 w-6">
-      <CircleHelp className="text-accent hover:text-accent-hover" />
-    </button>
-  );
-}
