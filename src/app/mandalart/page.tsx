c9e432984834229851f374fd6adefe49f5f0aa8d
kotarokomori <komopro.works@gmail.com>
2024-09-21 02:43:53 +0900

    各セルのテーマの入力簡易化

--- src/app/mandalart/page.tsx
+++ src/app/mandalart/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { MandalartLayout, MandalartNav, MandalartTable } from '@/features/mandalart';
 import { initialTableData, TableData } from '@/data/initialTableData';
+import { useState } from 'react';
 
 export default function Mandalart() {
+  const [tableThemeData, setTableThemeData] = useState<string[]>(Array(7).fill(''));
+
   return (
     <>
       <header className="absolute bottom-19 right-1/24 translate-x-1/2 transform">
@@ -10,7 +15,13 @@ export default function Mandalart() {
       <main className="mx-auto flex flex-grow" style={{ width: 'calc(83.333333% + 0.5rem)' }}>
         <MandalartLayout>
           {initialTableData.map((table: TableData) => (
-            <MandalartTable key={table.id} tableIndex={table.id} tableData={table.cells} />
+            <MandalartTable
+              key={table.id}
+              tableIndex={table.id}
+              tableData={table.cells}
+              tableThemeData={tableThemeData}
+              setTableThemeData={setTableThemeData}
+            />
           ))}
         </MandalartLayout>
       </main>
--- src/features/mandalart/mandalartTable.tsx
+++ src/features/mandalart/mandalartTable.tsx
@@ -4,10 +4,12 @@ import { TableCellData } from '@/data/initialTableData';
 type Props = {
   tableIndex: number;
   tableData: TableCellData[][];
+  tableThemeData: string[];
+  setTableThemeData: (data: string[]) => void;
 };
 
 export default function MandalartTable(props: Props) {
-  const { tableIndex, tableData } = props;
+  const { tableIndex, tableData, tableThemeData, setTableThemeData } = props;
 
   return (
     <table className="w-full table-fixed border-separate border-spacing-0 overflow-hidden rounded-sm border">
@@ -15,7 +17,13 @@ export default function MandalartTable(props: Props) {
         {tableData.map((row, rowIndex) => (
           <tr key={rowIndex} className="h-1/3 border">
             {row.map((cell) => (
-              <TableCell key={cell.id} tableIndex={tableIndex} cellData={cell} />
+              <TableCell
+                key={cell.id}
+                tableIndex={tableIndex}
+                cellData={cell}
+                tableThemeData={tableThemeData}
+                setTableThemeData={setTableThemeData}
+              />
             ))}
           </tr>
         ))}
